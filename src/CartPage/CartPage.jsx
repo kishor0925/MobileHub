dac0c48c9b08d78536473cb342e0f1ab48e4c650
kishor0925 <kishor25092004@gmail.com>
2026-03-14 23:35:11 +0530

    changes

--- src/CartPage/CartPage.jsx
+++ src/CartPage/CartPage.jsx
@@ -6,15 +6,88 @@ const CartPage = () => {
   const { cartItems } = useSelector(state => state.cart)
 
   return (
-    <div className='container'>
-      {cartItems.map((pro) => (
-        <div key={pro.id}>
-          <h1>{pro.id}</h1>
-          <img src={pro.img} alt="" />
-          <p>Qty: {pro.quantity}</p>
+     <div className='container'>
+            <div className='row'>
+                <div className='col'>
+                    {cartItems.length === 0 ? (<p>Your Cart is Empty</p>) : (
+                        <table className='table table-bordered text-center align-middle'>
+                            <thead className="table-dark">
+                                <tr>
+                                    <th>Image</th>
+                                    <th>Product Name</th>
+                                    <th>Price</th>
+                                    <th>Quantity</th>
+                                    <th>Update</th>
+                                    <th>Total</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                {cartItems.map((product, index) => (
+                                    <tr key={index}>
+                                        <td>
+                                            <img
+                                                src={product.img}
+                                                alt={product.title}
+                                                style={{
+                                                    width: "80px",
+                                                    height: "80px",
+                                                    objectFit: "cover",
+                                                    borderRadius: "8px"
+                                                }}
+                                            />
+                                        </td>
+
+                                        <td>{product.title}</td>
+
+                                        <td>₹{product.price}</td>
+
+                                        <td>{product.quantity}</td>
+
+                                        <td>
+                                            <button
+                                               
+                                                className="btn btn-sm btn-secondary"
+                                            >
+                                                -
+                                            </button>
+
+                                            <span className="mx-2">
+                                                {product.quantity}
+                                            </span>
+
+                                            <button
+                                                
+                                                className="btn btn-sm btn-secondary"
+                                            >
+                                                +
+                                            </button>
+                                        </td>
+
+                                        <td>₹{product.price * product.quantity}</td>
+
+                                        <td>
+                                            <button
+                                               
+                                                className="btn btn-sm btn-danger"
+                                            >
+                                                Remove
+                                            </button>
+                                        </td>
+                                    </tr>
+                                ))}
+                                
+                            </tbody>
+                        </table>
+
+
+                    )
+                    }
+                </div>
+            </div>
         </div>
-      ))}
-    </div>
+
   )
 }
 
--- src/slice/cartSlice.js
+++ src/slice/cartSlice.js
@@ -8,22 +8,20 @@ const cartSlice =  createSlice({
     name : 'cart',
     initialState,
     reducers : {
-         addtoCart(state, action) {
+        addtoCart(state , action){
+            const pid = action.payload;
+            const existingItem = state.cartItems.find( i => i.id === pid.id);
 
-            const item = action.payload;
-            const existingItem = state.cartItems.find(i => i.id === item.id);
-            if(existingItem)
-            {
+            if(existingItem){
                 existingItem.quantity += 1;
             }
             else
             {
-                state.cartItems.push({...item, quantity : 1});
+                state.cartItems.push({...state, quantity : 1});
             }
-
-            localStorage.setItem('cartItems', JSON.stringify(state.cartItems))
-
+            localStorage.setItem('cartItem' , JSON.stringify(state.cartItems));
         }
+        
     }
 })
 
